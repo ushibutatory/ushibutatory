e1c7a9c10114e6c1d99082412bccd9f58c302e1a
ushibutatory <ushibutatory@gmail.com>
2023-10-02 01:35:13 +0900

    refactor

--- docs/main.js
+++ docs/main.js
@@ -1,27 +1,51 @@
 (() => {
   function loadCards($cardDeck, data) {
     data.forEach((_) => {
-      const title = document.createElement("a");
-      title.classList = "card-title link-offset-3";
-      title.text = _.title;
-      title.href = _.href;
-      title.target = "_blank";
-
-      const text = document.createElement("p");
-      text.classList = "cart-text";
-      text.innerText = _.text;
-
-      const cardHeader = document.createElement("div");
-      cardHeader.classList = "card-header border-dark";
-      cardHeader.append(title);
-
-      const cardBody = document.createElement("div");
-      cardBody.classList = "card-body";
-      cardBody.append(text);
-
       const card = document.createElement("div");
       card.classList = "card text-bg-dark border-light";
-      card.append(cardHeader, cardBody);
+      card.append(
+        (() => {
+          const cardHeader = document.createElement("div");
+          cardHeader.classList = "card-header border-dark";
+          cardHeader.append(
+            (() => {
+              const title = document.createElement("a");
+              {
+                title.classList = "card-title link-offset-3";
+                title.href = _.href;
+                title.target = "_blank";
+
+                if (_.src) {
+                  const icon = document.createElement("img");
+                  icon.src = _.src;
+                  title.append(icon);
+                }
+
+                const text = document.createElement("span");
+                text.innerText = _.title;
+                title.append(text);
+              }
+              return title;
+            })()
+          );
+          return cardHeader;
+        })(),
+        (() => {
+          const cardBody = document.createElement("div");
+          cardBody.classList = "card-body";
+          cardBody.append(
+            (() => {
+              const text = document.createElement("p");
+              {
+                text.classList = "cart-text";
+                text.innerText = _.text;
+              }
+              return text;
+            })()
+          );
+          return cardBody;
+        })()
+      );
       $cardDeck.append(card);
     });
   }
@@ -34,13 +58,13 @@
       href: "https://ushibutatory.hateblo.jp/",
     },
     {
-      src: "images/",
+      src: "",
       title: "Twitter / X",
       text: "日々の呟きです。",
       href: "https://twitter.com/ushibutatory",
     },
     {
-      src: "images/",
+      src: "",
       title: "Instagram",
       text: "いいなぁと思ったものを撮っています。",
       href: "https://www.instagram.com/ushibutatory/",
@@ -49,7 +73,7 @@
 
   loadCards($("#cards_tech"), [
     {
-      src: "images/github.png",
+      src: "",
       title: "GitHub",
       text: "個人開発や技術検証に使用しています。",
       href: "https://github.com/ushibutatory",
@@ -70,8 +94,8 @@
 
   loadCards($("#cards_artifact"), [
     {
-      src: "images/",
-      title: "ウマ娘誕生日カレンダー",
+      src: "",
+      title: "ウマ娘\r\n誕生日カレンダー",
       text: "各種カレンダーアプリで表示できるように、iCalendarを配信しています。",
       href: "https://umamusume-birthdays.ushibutatory.net/",
     },
